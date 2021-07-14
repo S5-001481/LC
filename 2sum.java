45b77714ae733579b695270750e2a53f3e8c1f1f
Jane Z <40412341+S5-001481@users.noreply.github.com>
2021-07-14 17:12:10 +0900

    Update 2sum.java

--- 2sum.java
+++ 2sum.java
@@ -1,14 +1,11 @@
-import java.util.HashMap;
+import java.util.HashMap;/*H和M大写*/
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> inverse = new HashMap<Integer, Integer>();
         for(int i = 0; i< nums.length; i++){
 			if(inverse.containsKey(nums[i])){
-				int[] ans = new int[2];
-				ans[0] = i;
-				ans[1] = inverse.get(nums[i]);
-				return ans;
+				return new int[]{i,(inverse.get(nums[i]))}; 	/*new是这个格式*/
 			}
 			inverse.put((target - nums[i]),i);
 		}
